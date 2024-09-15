35c38fcd63a9c9be5195d667ff6ab1d4e06fbf5e
2743-123 <2743-123>
2024-09-15 11:24:23 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+async function getpost(params) {
+    try{
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+        let data = await response.json();
+        console.log(data);
+    }catch(error){
+        console.log('Error',error);
+    }
+}
+getpost();
